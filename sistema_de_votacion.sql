b6c9bba80f5a7f50c3960448d886106107f106bd
jesusflorez3033-dev <jesusflorez3033@gmail.com>
2026-04-21 19:19:42 -0500

    Add files via upload

---
+++ sistema_de_votacion.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-04-2026 a las 02:17:39
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema de votacion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `candidato`
+--
+
+CREATE TABLE `candidato` (
+  `candidato_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `propuesta` text NOT NULL,
+  `facultad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cargo`
+--
+
+CREATE TABLE `cargo` (
+  `cargo_id` int(11) NOT NULL,
+  `presidente` varchar(50) NOT NULL,
+  `secretario` varchar(50) NOT NULL,
+  `tesorero` varchar(50) NOT NULL,
+  `candidato_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `estudiante_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` text NOT NULL,
+  `facultad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facultad`
+--
+
+CREATE TABLE `facultad` (
+  `facultad_id` int(11) NOT NULL,
+  `nombra` varchar(50) NOT NULL,
+  `decano` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `presidente`
+--
+
+CREATE TABLE `presidente` (
+  `presidente_id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `apellido` int(11) NOT NULL,
+  `cargo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `voto`
+--
+
+CREATE TABLE `voto` (
+  `voto_id` int(11) NOT NULL,
+  `estudiante_id` int(11) NOT NULL,
+  `candidato_id` int(11) NOT NULL,
+  `fecha_hora` date NOT NULL,
+  `ip_registro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `candidato`
+--
+ALTER TABLE `candidato`
+  ADD PRIMARY KEY (`candidato_id`),
+  ADD UNIQUE KEY `facultad_id` (`facultad_id`);
+
+--
+-- Indices de la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD PRIMARY KEY (`cargo_id`),
+  ADD UNIQUE KEY `candidato_id` (`candidato_id`);
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`estudiante_id`),
+  ADD UNIQUE KEY `facultad_id` (`facultad_id`);
+
+--
+-- Indices de la tabla `facultad`
+--
+ALTER TABLE `facultad`
+  ADD PRIMARY KEY (`facultad_id`);
+
+--
+-- Indices de la tabla `presidente`
+--
+ALTER TABLE `presidente`
+  ADD PRIMARY KEY (`presidente_id`);
+
+--
+-- Indices de la tabla `voto`
+--
+ALTER TABLE `voto`
+  ADD PRIMARY KEY (`voto_id`),
+  ADD UNIQUE KEY `estudiante_id` (`estudiante_id`),
+  ADD UNIQUE KEY `candidato_id` (`candidato_id`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `candidato`
+--
+ALTER TABLE `candidato`
+  ADD CONSTRAINT `candidato_ibfk_1` FOREIGN KEY (`facultad_id`) REFERENCES `facultad` (`facultad_id`);
+
+--
+-- Filtros para la tabla `cargo`
+--
+ALTER TABLE `cargo`
+  ADD CONSTRAINT `cargo_ibfk_2` FOREIGN KEY (`candidato_id`) REFERENCES `candidato` (`candidato_id`);
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`facultad_id`) REFERENCES `facultad` (`facultad_id`);
+
+--
+-- Filtros para la tabla `voto`
+--
+ALTER TABLE `voto`
+  ADD CONSTRAINT `voto_ibfk_1` FOREIGN KEY (`candidato_id`) REFERENCES `candidato` (`candidato_id`),
+  ADD CONSTRAINT `voto_ibfk_2` FOREIGN KEY (`estudiante_id`) REFERENCES `estudiante` (`estudiante_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
